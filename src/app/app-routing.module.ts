343dde3ddd2699879da95a7866c06297abfbc81e
majmurek <majmurek@wszib.edu.pl>
2020-03-01 16:23:57 +0100

    front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {ShopComponent} from "./shop/shop.component";
+import {ProductsComponent} from "./shop/products/products.component";
+import {ShippingComponent} from "./shop/shipping/shipping.component";
+import {EditorComponent} from "./shop/products/editor/editor.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'shop', component: ShopComponent, children: [
+      {path: '', component: ProductsComponent},
+      {path: 'editor', component: EditorComponent},
+      {path: 'editor/:id', component: EditorComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'products/editor', component: EditorComponent},
+      {path: 'products/editor/:id', component: EditorComponent},
+      {path: 'shipping', component: ShippingComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ShopComponent } from './shop/shop.component';
+import { ShippingComponent } from './shop/shipping/shipping.component';
+import { ProductsComponent } from './shop/products/products.component';
+import { EditorComponent } from './shop/products/editor/editor.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ShopComponent,
+    ShippingComponent,
+    ProductsComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
